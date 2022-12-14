17607a7673797640253d499d144edcb09c3f4de4
Hailey Francis <n01402670@unf.edu>
2022-12-14 15:16:30 -0500

    API key authorization recieved, refactor fetch url, begin displaying current date info

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,22 +7,28 @@ const API_KEY = "861f2cb6b96250af24f566edb2fe2923";
 function search(){
     var searchInput = $("#search-input").val();
     addToList(searchInput);
+    fetchAPI(searchInput);
     $("#search-input").val("");
-    
-    
-    
-
 }
 
-function fetchAPI(searchInput){
-    fetch("https://api.openweathermap.org/data/2.5/weather?q={" + searchInput  + "}&appid={" + API_KEY + "}")
-    
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        console.log(data);
-    });
+function fetchAPI(input){
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + input + "&appid=" + API_KEY;
+    fetch(queryURL)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data);
+            displayToday(data);
+        });
+}
+
+function displayToday(city){
+    $("#today-city-name").text("City: " + city.name + " (date)");
+    $("#today-city-temp").text("Temperature: " + fahrenheit(city.main.temp) + '\u00B0F');
+    $("#today-city-wind").text("Wind: " + city.wind.speed);
+    $("#today-city-humidity").text("Humidity: " + city.main.humidity);
+    console.log("yes");
 }
 
 function addToList(city){
@@ -30,6 +36,10 @@ function addToList(city){
     //likely will use local storage
     $("#city-list").append("<li><button type=\"button\" id=\"" + city + "\">" + city + "</button></li>");
 
+}
+
+function fahrenheit(kelvin){
+    return Math.round(1.8 * (kelvin - 273.15) + 32);
 }
 
 $("#search-btn").click(search);
